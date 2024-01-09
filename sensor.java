5b8e3b5e020b410a54238a15975bbcaf36e98ab7
Utkarsh1343 <ahmednagaruser414006@gmail.com>
2024-01-09 23:24:59 +0530

    sensor.jawa made

---
+++ sensor.java
@@ -0,0 +1,12 @@
+ public class sensor{
+public static int main(String[] args,int p) {
+   double value = Math.random();
+   if(value>p)
+   {
+    return 1;
+   }
+   else
+   {
+    return 0;
+   }
+}}
